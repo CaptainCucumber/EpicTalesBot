1c4c12680345f24193c43ddf13582aa1a6231a20
Capitan Cucumber 🥒 <haughty.marshes_0c@icloud.com>
2023-12-23 21:43:53 -0800

    Leave unapproved channels
    Validate access to chat per message basis. Since we don't know if anyone
    has already added the bot to the channel.

--- bot/config.py
+++ bot/config.py
@@ -1,11 +1,9 @@
-import json
 import os
-import random
 
-phrases = []
+
+WHITELISTED_GROUPS = [-4084203084, -809833778]
 
 class Config:
-
     @staticmethod
     def get_telegram_token():
         return os.environ.get('TELEGRAM_TOKEN', 'default_telegram_token')
@@ -14,12 +12,3 @@ class Config:
     def get_google_service_file():
         return os.environ.get('GOOGLE_SERVICE_FILE', 'default_service_file')
 
-    @staticmethod
-    def get_generic_error():
-        global phrases
-
-        if not phrases:
-            with open('phrases.json', 'r', encoding='utf-8') as file:
-                phrases = json.load(file)
-
-        return random.choice(phrases)
