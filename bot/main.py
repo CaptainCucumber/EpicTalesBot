56355764e80f0c767d5d5eb77a2e109350b37981
Capitan Cucumber 🥒 <haughty.marshes_0c@icloud.com>
2023-12-25 22:09:17 -0800

    Replies to the original message.
    Slightly refactor the code to avoid pilling up everything into a
    single function
    
    - close #3

--- bot/main.py
+++ bot/main.py
@@ -4,7 +4,7 @@ from logging.handlers import RotatingFileHandler
 import re
 
 import requests
-from telegram import Update
+from telegram import Message, Update
 from telegram.ext import ApplicationBuilder, CallbackContext, MessageHandler, filters
 
 from article_gpt import ArticleGPT
@@ -38,35 +38,55 @@ async def download_voice(voice_file_id, context):
 async def handle_messages(update: Update, context: CallbackContext) -> None:
     message = update.message
 
-    # If it's a group message and the group is not whitelisted, leave the group
-    if message.chat.id not in Config.WHITELISTED_GROUPS:
-        await context.bot.leave_chat(message.chat.id)
-        logger.warning(f"Leaving non-whitelisted group with chat ID {message.chat.id}, message type: {message.chat.type} and message: {message.text}")
+    if not is_whitelisted(message):
+        await leave_group(context, message)
         return
 
-    # Check if the bot is mentioned and if the message is a reply
-    if message.reply_to_message and f'@{context.bot.username}' in message.text:
+    if is_bot_mentioned(context.bot.username, message):
+        await process_messages(context, message)
 
-        replied_message = message.reply_to_message
 
-        # Extract the username or first name of the user who sent the replied message
-        user = replied_message.from_user
-        user_name = user.first_name or user.username  # Prefer first name, fallback to user name
+async def leave_group(context: CallbackContext, message: Message) -> None:
+    await context.bot.leave_chat(message.chat.id)
+    logger.warning(
+        f"Leaving non-whitelisted group with chat ID {message.chat.id}, "
+        f"message type: {message.chat.type} and message: {message.text}"
+    )
 
-        # Handle voice messages
-        if replied_message.voice:
-            file = await context.bot.get_file(replied_message.voice.file_id)
-            file_url = file.file_path
 
-            transcription = await stt.transcribe_voice(file_url)
-            await message.reply_text(f"{user_name} сказал '{transcription}'")
-        elif replied_message.text:
-            url_pattern = r'https?://[^\s]+'
-            url = re.findall(url_pattern, replied_message.text)
-            if url:
-                text = replied_message.text
-                summary = article_gpt.summarize(text)
-                await message.reply_to_message.reply_text(summary)
+def is_whitelisted(message: Message) -> bool:
+    return message.chat.id in Config.WHITELISTED_GROUPS
+
+
+def is_bot_mentioned(botname: str, message: Message) -> bool:
+    return message.reply_to_message and f'@{botname}' in message.text
+
+
+async def process_messages(context: CallbackContext, message: Message) -> None:
+    replied_message = message.reply_to_message
+
+    if replied_message.voice:
+        await handle_voice_message(context, replied_message)
+    elif replied_message.text:
+        await handle_text_message(context, replied_message)
+
+
+async def handle_voice_message(context: CallbackContext, message: Message) -> None:
+    file = await context.bot.get_file(message.voice.file_id)
+    file_url = file.file_path
+
+    transcription = await stt.transcribe_voice(file_url)
+    await message.reply_text(transcription)
+
+
+async def handle_text_message(context: CallbackContext, message: Message) -> None:
+    url_pattern = r'https?://[^\s]+'
+    urls = re.findall(url_pattern, message.text)
+
+    if urls:
+        text = message.text
+        summary = article_gpt.summarize(text)
+        await message.reply_text(summary)
 
 
 def main() -> None:
