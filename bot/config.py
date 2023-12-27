07fa82cb1ed0687a9aedd4e03281736f3243f95f
Capitan Cucumber 🥒 <haughty.marshes_0c@icloud.com>
2023-12-27 14:36:23 -0800

    Buy buy Google Speech-to-text and welcome Whisper
    Having so many problems with Google STT and spacy together to get
    proper contextualized response. I'd rather use Whisper API. Althought
    it's much more heavyweight and requires separate libraries installed
    on the machine, the final result is so much better.
    
    Maybe one day we can port back to spacy and Google STT to reduce cost.

--- bot/config.py
+++ bot/config.py
@@ -9,9 +9,6 @@ class Config:
 
     def get_telegram_token(self):
         return self._env['TELEGRAM_TOKEN']
-
-    def get_google_service_file(self):
-        return self._env['GOOGLE_SERVICE_FILE']
     
     def get_open_ai_key(self):
         return self._env['OPENAI_KEY']
--- bot/stt.py
+++ bot/stt.py
@@ -1,33 +1,48 @@
 import requests
-from google.cloud import speech
-from google.oauth2 import service_account
-
+import openai
+from pydub import AudioSegment
+import io
+import whisper
 from config import Config
-
+import tempfile
+import os
 
 class STT:
     def __init__(self, config: Config) -> None:
-        service_file = config.get_google_service_file()
-        credentials = service_account.Credentials.from_service_account_file(service_file)
-
-        self._client = speech.SpeechClient(credentials=credentials)
-        self._config = speech.RecognitionConfig(
-            encoding=speech.RecognitionConfig.AudioEncoding.OGG_OPUS,
-            sample_rate_hertz=48000,
-            language_code="ru-RU",
-            enable_word_confidence=True
-        )
+        openai.api_key = config.get_open_ai_key()
+        self.whisper_model = whisper.load_model("base")  # Load Whisper model
 
     def _download_audio(self, voice_file_id: str) -> bytes:
         response = requests.get(voice_file_id)
         return response.content
 
-    async def transcribe_voice(self, url: bytes) -> str:
+    def _convert_oga_to_wav(self, oga_audio_data: bytes) -> io.BytesIO:
+        oga_audio_stream = io.BytesIO(oga_audio_data)
+        audio = AudioSegment.from_ogg(oga_audio_stream)
+
+        wav_audio_stream = io.BytesIO()
+        audio.export(wav_audio_stream, format="wav")
+        wav_audio_stream.seek(0)
+
+        return wav_audio_stream
+
+    async def transcribe_voice(self, url: str) -> str:
         voice_data = self._download_audio(url)
-        audio = speech.RecognitionAudio(content=voice_data)
 
-        response = self._client.recognize(config=self._config, audio=audio)
-        return ' '.join([result.alternatives[0].transcript for result in response.results])
+        # Convert OGA to WAV
+        wav_audio_stream = self._convert_oga_to_wav(voice_data)
 
+        # Save the WAV audio to a temporary file
+        with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as temp_file:
+            temp_file.write(wav_audio_stream.read())
+            temp_file_path = temp_file.name
 
+        # Transcribe using Whisper
+        audio_data = whisper.load_audio(temp_file_path)
+        result = self.whisper_model.transcribe(audio_data)
+        voice_text = result["text"]
 
+        # Clean up: delete the temporary file
+        os.remove(temp_file_path)
+
+        return voice_text
