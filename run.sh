94f683f816e8475e82e358f220a0b4d04a4f0025
Capitan Cucumber 🥒 <haughty.marshes_0c@icloud.com>
2024-01-01 22:14:05 -0800

    Keep .mo files up-to-date when run the script.
    Just to avoid human error. Compile the files before the start.

--- run.sh
+++ run.sh
@@ -6,4 +6,15 @@ set -e
 # Read environment variables from config.env
 source config.env
 
+# Compile .po files to .mo files for localization
+function compile_locales() {
+    for lang in bot/locales/*/LC_MESSAGES; do
+        msgfmt "$lang"/*.po -o "$lang"/epictales.mo
+    done
+}
+
+# Call the compile_locales function
+compile_locales
+
+# Run the Python application
 .venv/bin/python ./bot/main.py
